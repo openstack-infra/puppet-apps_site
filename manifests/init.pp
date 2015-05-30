93c85e96bfab5189509de9a0b77aa6207289a165
Christopher Aedo <doc@aedo.net>
2015-05-30 13:22:52 -0700

    Create site for apps.openstack.org
    This commit adds the puppet module to
    host the content of apps.openstack.org
    by cloning the repo hosting static
    content.
    
    Change-Id: If419130e5ed01742c0e3840c7fe761ca53f86560

---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+# == Class: apps_site
+#
+class apps_site (
+  $vhost_name = $::fqdn,
+  $root_dir = '/opt/apps_site',
+  $serveradmin = "webmaster@${::domain}",
+
+  $commit = 'master',
+) {
+
+  if !defined(Package['git']) {
+    package { 'git':
+      ensure => present
+    }
+  }
+
+  vcsrepo { $root_dir:
+    ensure   => latest,
+    provider => git,
+    revision => $commit,
+    source   => 'https://git.openstack.org/stackforge/apps-catalog.git',
+    require  => [
+      Package['git'],
+    ]
+  }
+
+  include apache
+
+  apache::vhost { $vhost_name:
+    port       => 80,
+    docroot    => "${root_dir}/openstack_catalog/web",
+    priority   => '50',
+    template   => 'apps_site/vhost.erb',
+    vhost_name => $vhost_name,
+  }
+
+}
