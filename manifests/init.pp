f8da03f710e72d78207973759b55ee5558e8b5f8
Christopher Aedo <doc@aedo.net>
2015-06-23 16:36:48 -0700

    Enable CORS for app-catalog
    This commit enables the apache headers module,
    and adds an access control header which will
    allow AJAX requests from other sites. This
    is intended to allow a Horizon panel to work
    with the App Catalog directly.
    
    This commit also includes a modification to
    the puppet modeule to create three JSON files from
    the contents of YAML files retrieved from the
    App Catalog repository.
    
    Change-Id: I4a3c44bb5806362416b1b175d917939ebaa351dd
    Closes-Bug: 1467699

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,6 +25,7 @@ class apps_site (
   }
 
   include apache
+  include pip
 
   apache::vhost { $vhost_name:
     port       => 80,
@@ -33,5 +34,34 @@ class apps_site (
     template   => 'apps_site/vhost.erb',
     vhost_name => $vhost_name,
   }
+
+  a2mod { 'headers':
+    ensure => present
+  }
+
+  if ! defined(Package['python-yaml']) {
+    package { 'python-yaml':
+      ensure => present,
+    }
+  }
+
+  exec { 'make_glance_json' :
+    command   => "python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout)' < ${root_dir}/openstack_catalog/web/static/glance_images.yaml > ${root_dir}/openstack_catalog/web/static/glance_images.json",
+    path      => '/usr/local/bin:/usr/bin:/bin',
+    subscribe => Vcsrepo[$root_dir],
+  }
+
+  exec { 'make_heat_json' :
+    command   => "python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout)' < ${root_dir}/openstack_catalog/web/static/heat_templates.yaml > ${root_dir}/openstack_catalog/web/static/heat_templates.json",
+    path      => '/usr/local/bin:/usr/bin:/bin',
+    subscribe => Vcsrepo[$root_dir],
+  }
+
+  exec { 'make_murano_json' :
+    command   => "python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout)' < ${root_dir}/openstack_catalog/web/static/murano_apps.yaml > ${root_dir}/openstack_catalog/web/static/murano_apps.json",
+    path      => '/usr/local/bin:/usr/bin:/bin',
+    subscribe => Vcsrepo[$root_dir],
+  }
+
 
 }
