67b3ab0ab226793de403b842b3754486847a7450
Christopher Aedo <doc@aedo.net>
2015-09-23 13:38:11 -0700

    Add SSL support for apps.openstack.org.
    This commit enables SSL support for the
    App Catalog.
    
    Partial-Bug: 1496611
    Change-Id: I87180f34d583b7319e4964d8e97c53279554f3f3

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,13 @@ class apps_site (
   $vhost_name = $::fqdn,
   $root_dir = '/opt/apps_site',
   $serveradmin = "webmaster@${::domain}",
-
   $commit = 'master',
+  $ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+  $ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key',
+  $ssl_chain_file = undef,
+  $ssl_cert_file_contents = undef, # If left empty puppet will not create file.
+  $ssl_key_file_contents = undef, # If left empty puppet will not create file.
+  $ssl_chain_file_contents = undef, # If left empty puppet will not create file.
 ) {
 
   if !defined(Package['git']) {
@@ -27,11 +32,12 @@ class apps_site (
   include ::httpd
 
   ::httpd::vhost { $vhost_name:
-    port       => 80,
+    port       => 443,
     docroot    => "${root_dir}/openstack_catalog/web",
     priority   => '50',
     template   => 'apps_site/vhost.erb',
     vhost_name => $vhost_name,
+    ssl        => true,
   }
 
   httpd_mod { 'headers':
@@ -47,6 +53,37 @@ class apps_site (
   httpd_mod { 'deflate':
     ensure => present,
     notify => Service['httpd']
+  }
+
+  if $ssl_cert_file_contents != undef {
+    file { $ssl_cert_file:
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0640',
+      content => $ssl_cert_file_contents,
+      before  => httpd::vhost[$vhost_name],
+    }
+  }
+
+  if $ssl_key_file_contents != undef {
+    file { $ssl_key_file:
+      owner   => 'root',
+      group   => 'ssl-cert',
+      mode    => '0640',
+      content => $ssl_key_file_contents,
+      require => Package['ssl-cert'],
+      before  => httpd::vhost[$vhost_name],
+    }
+  }
+
+  if $ssl_chain_file_contents != undef {
+    file { $ssl_chain_file:
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0640',
+      content => $ssl_chain_file_contents,
+      before  => httpd::vhost[$vhost_name],
+    }
   }
 
   if ! defined(Package['python-yaml']) {
