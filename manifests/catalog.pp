b783ba30bb6639044395eeefb907b86c7b8d08ac
Bartosz Kupidura <bkupidura@mirantis.com>
2016-08-23 08:30:44 +0000

    [wip] Glare support for app-catalog
    This commit should allow to deploy app-catalog with glare support
    introduced by Ib5c6920c90f566d73a5f9e8ed545f0e082a0b20e
    
    Change-Id: I9fbe756cfcc2456587c0395e88f14681a1e43ee4

---
+++ manifests/catalog.pp
@@ -0,0 +1,36 @@
+class apps_site::catalog (
+  $settings_dir    = '/etc/openstack-catalog',
+  $memcache_server = '127.0.0.1:11211',
+  $domain          = $::fqdn,
+  $port            = 80,
+  $glare_url       = 'http://127.0.0.1:9494'
+) {
+
+  include apps_site
+
+  #settings_dir should be set /etc/openstack-catalog
+  #currently app-catalog will not use env variables properly
+  file { $settings_dir:
+    ensure => 'directory',
+  }
+
+  file { "${settings_dir}/local_settings.py":
+    ensure  => 'present',
+    content => template('apps_site/local_settings.erb'),
+    require => File[$settings_dir],
+  }
+
+  exec { 'app-catalog-collect-static' :
+    command     => 'app-catalog-manage collectstatic --noinput',
+    path        => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin', '/usr/local/sbin'],
+    refreshonly => true,
+    subscribe   => [Package['openstack-app-catalog'], File["${settings_dir}/local_settings.py"]],
+  }
+
+  exec { 'app-catalog-compress' :
+    command     => 'app-catalog-manage compress --force',
+    path        => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin', '/usr/local/sbin'],
+    refreshonly => true,
+    subscribe   => [Package['openstack-app-catalog'], File["${settings_dir}/local_settings.py"]],
+  }
+}
---
+++ manifests/plugins/glare.pp
@@ -0,0 +1,23 @@
+# == Class: apps_site::plugins::glare
+#
+class apps_site::plugins::glare (
+  $glare_url     = 'http://127.0.0.1:9494/',
+  $assets_file   = undef,
+  $import_assets = false,
+) inherits apps_site::params {
+
+  include apps_site
+
+  $real_assets_file = $assets_file ? {
+    undef   => "${apps_site::params::app_catalog_dir}/web/static/assets.yaml",
+    default => $assets_file,
+  }
+
+  if $import_assets {
+    exec { 'import-glare-assets' :
+      command   => "app-catalog-import-assets --glare_url ${glare_url} --assets_file ${real_assets_file}",
+      path      => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin', '/usr/local/sbin'],
+    }
+  }
+
+}
