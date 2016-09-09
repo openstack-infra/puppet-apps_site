8b72875d3c1f95767737c4d961f035e5782ff880
Bartosz Kupidura <bkupidura@mirantis.com>
2016-09-09 14:47:43 +0000

    Glare support for app-catalog
    This commit should allow to deploy app-catalog with glare support
    introduced by Ib5c6920c90f566d73a5f9e8ed545f0e082a0b20e
    
    Change-Id: I9fbe756cfcc2456587c0395e88f14681a1e43ee4

---
+++ examples/app_site_with_glare_using_git.pp
@@ -0,0 +1,48 @@
+#if you want to install app_site with glare support via git, memcache installed on localhost
+$vhost_name = $::fqdn
+$without_glare = false
+$glare_server = "${vhost_name}:9494"
+$memcache_server = '127.0.0.1:11211'
+$repo_url = 'https://github.com/openstack/app-catalog.git'
+$commit = 'master'
+$use_ssl = false
+
+if $use_ssl {
+  $glare_url = "https://${glare_server}"
+}else{
+  $glare_url = "http://${glare_server}"
+}
+
+#installation with importing assets
+$import_assets = true
+
+class { '::apps_site':
+  without_glare   => $without_glare,
+  use_pip         => false,
+  use_git         => true,
+  repo_url        => $repo_url,
+  commit          => $commit,
+}
+
+class { '::apps_site::plugins::glare':
+  use_ssl         => $use_ssl,
+  memcache_server => $memcache_server,
+  vhost_name      => $vhost_name,
+}
+
+class { '::apps_site::wsgi::apache':
+  use_ssl    => $use_ssl,
+  servername => $vhost_name,
+}
+
+class { '::apps_site::catalog':
+  import_assets   => $import_assets,
+  domain          => $vhost_name,
+  glare_url       => $glare_url,
+  memcache_server => $memcache_server,
+}
+
+Class['::apps_site'] ->
+  Class['::apps_site::plugins::glare'] ->
+      Class['::apps_site::wsgi::apache'] ->
+        Class['::apps_site::catalog']
---
+++ manifests/plugins/glare.pp
@@ -0,0 +1,107 @@
+# class: apps_site::plugins::glare
+#
+class apps_site::plugins::glare (
+  $vhost_name                = $::fqdn,
+  $memcache_server           = '127.0.0.1:11211',
+  $cookie_name               = 's.aoo',
+  $use_ssl                   = false,
+  $ssl_cert_file_content     = undef,
+  $ssl_key_file_content      = undef,
+  $ssl_ca_file_content       = undef,
+  $ssl_cert_file_location    = '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+  $ssl_key_file_location     = '/etc/ssl/private/ssl-cert-snakeoil.key',
+  $ssl_ca_file_location      = '/etc/ssl/certs/ca-certificates.crt',
+) inherits ::apps_site::params {
+
+  package { 'glare_dev':
+    ensure   => present,
+    provider => 'pip',
+  }
+
+  service { 'glare-api':
+    ensure   => 'running',
+    provider => base,
+    start    => 'nohup /usr/local/bin/glare-api --config-file /usr/local/etc/glare/glare.conf &',
+    restart  => 'killall glare-api; nohup /usr/local/bin/glare-api --config-file /usr/local/etc/glare/glare.conf &',
+    stop     => 'killall glare-api',
+  }
+
+  Package['glare_dev'] -> Service['glare-api']
+#  include ::glare::params
+#  include ::glare::db::sync
+#
+#  if $use_ssl {
+#    if $ssl_cert_file_content != undef {
+#      file { $ssl_cert_file_location:
+#        owner   => 'root',
+#        group   => 'root',
+#        mode    => '0640',
+#        content => $ssl_cert_file_content,
+#      }
+#    }
+#
+#    if $ssl_key_file_content != undef {
+#      file { $ssl_key_file_location:
+#        owner   => 'root',
+#        group   => 'ssl-cert',
+#        mode    => '0640',
+#        content => $ssl_key_file_content,
+#      }
+#    }
+#
+#    if $ssl_ca_file_content != undef {
+#      file { $ssl_ca_file_location:
+#        owner   => 'root',
+#        group   => 'root',
+#        mode    => '0640',
+#        content => $ssl_ca_file_content,
+#      }
+#    }
+#  }
+#
+#  #at the moment the package is located in the experimental repository
+#  #for debian, should be removed when it will be switched to stable
+#  if $::os_package_type == 'debian'{
+#    Package<|title == 'glare'|> {
+#      name            => 'glare-api',
+#      install_options => ['-t', 'experimental'],
+#    }
+#  }
+#
+#   glare_paste_ini {
+#     'filter:session/paste.filter_factory':
+#       value => 'openstack_app_catalog.middlewares:SessionMiddleware.factory';
+#     'filter:session/memcached_server':
+#       value => $memcache_server;
+#     'filter:session/session_cookie_name':
+#       value => $cookie_name;
+#     'filter:cors/allowed_origin':
+#       value => "http://${vhost_name}";
+#   }
+#
+#   file { '/etc/glare/glare-policy.json':
+#     content => "{\n  \"context_is_admin\": \"role:app-catalog-core\"  \n}",
+#     require => Package[ 'glare' ],
+#   }
+#
+#   glare_config {
+#     'oslo_policy/policy_file': value => 'glare-policy.json';
+#   }
+#
+#   if $use_ssl {
+#     class { '::glare':
+#       pipeline               => 'session',
+#       allow_anonymous_access => true,
+#       auth_strategy          => 'none',
+#       cert_file              => $ssl_cert_file_location,
+#       key_file               => $ssl_key_file_location,
+#       ca_file                => $ssl_ca_file_location,
+#     }
+#   }else{
+#      class { '::glare':
+#        pipeline               => 'session',
+#        allow_anonymous_access => true,
+#        auth_strategy          => 'none',
+#      }
+#   }
+}
