fb94e864bb9dcfbd0ccf293a01fb8a9ef04178d0
Bartosz Kupidura <bkupidura@mirantis.com>
2016-09-09 14:47:43 +0000

    Glare support for app-catalog
    This commit should allow to deploy app-catalog with glare support
    introduced by Ib5c6920c90f566d73a5f9e8ed545f0e082a0b20e
    
    Change-Id: I9fbe756cfcc2456587c0395e88f14681a1e43ee4

---
+++ examples/app_site_with_glare_using_pip.pp
@@ -0,0 +1,24 @@
+#if you want to install app_site with glare support via pip, memcache installed on localhost
+$vhost_name = $::fqdn
+$without_glare = false
+$glare_url = 'http://127.0.0.1:9494'
+$memcache_server = '127.0.0.1:11211'
+
+#installation with importing assets
+$import_assets = true
+
+class { '::apps_site':
+  vhost_name      => $vhost_name,
+  without_glare   => $without_glare,
+  glare_url       => $glare_url,
+  memcache_server => $memcache_server,
+}
+
+class { '::apps_site::wsgi::apache':
+  servername => $vhost_name,
+}
+
+class { '::apps_site::plugins::glare':
+  import_assets => $import_assets,
+  glare_url     => $glare_url,
+}
