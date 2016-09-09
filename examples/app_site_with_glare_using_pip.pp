fbca91d9dd06a80bbc67867e0d426310f6dc8516
Bartosz Kupidura <bkupidura@mirantis.com>
2016-09-09 14:47:43 +0000

    Glare support for app-catalog
    This commit should allow to deploy app-catalog with glare support
    introduced by Ib5c6920c90f566d73a5f9e8ed545f0e082a0b20e
    
    Change-Id: I9fbe756cfcc2456587c0395e88f14681a1e43ee4

---
+++ examples/app_site_with_glare_using_pip.pp
@@ -0,0 +1,45 @@
+#if you want to install app_site with glare support via pip, memcache installed on localhost
+$vhost_name = $::fqdn
+$without_glare = false
+$glare_server = '127.0.0.1:9494'
+$memcache_server = '127.0.0.1:11211'
+$use_ssl = false
+
+if $use_ssl {
+  $glare_url = "https://${glare_server}"
+}else{
+  $glare_url = "http://${glare_server}"
+}
+
+#installation with importing assets
+$import_assets = true
+
+class { '::apps_site':
+  without_glare   => $without_glare,
+  glare_url       => $glare_url,
+  memcache_server => $memcache_server,
+}
+
+class { '::apps_site::plugins::glare':
+  glare_url       => $glare_url,
+  memcache_server => $memcache_server,
+  vhost_name      => $vhost_name,
+}
+
+class { '::apps_site::wsgi::apache':
+  use_ssl    => $use_ssl,
+  servername => $vhost_name,
+}
+
+class { '::apps_site::catalog':
+  import_assets   => $import_assets,
+  domain          => $vhost_name,
+  glare_url       => $glare_url,
+  memcache_server => $memcache_server,
+}
+
+Class['::apps_site'] ->
+  Class['::apps_site::plugins::glare'] ->
+    Anchor['glare::service::end'] ->
+      Class['::apps_site::wsgi::apache'] ->
+        Class['::apps_site::catalog']
---
+++ manifests/wsgi/apache.pp
@@ -0,0 +1,137 @@
+# == Class: apps_site::wsgi::apache
+#
+class apps_site::wsgi::apache (
+  $servername                = $::fqdn,
+  $http_port                 = 80,
+  $https_port                = 443,
+  $ssl_cert_file_content     = undef,
+  $ssl_key_file_content      = undef,
+  $ssl_ca_file_content       = undef,
+  $ssl_cert_file_location    = '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+  $ssl_key_file_location     = '/etc/ssl/private/ssl-cert-snakeoil.key',
+  $ssl_ca_file_location      = '/etc/ssl/certs/ca-certificates.crt',
+  $use_ssl                   = true,
+  $bind_ip                   = undef,
+  $settings_dir              = '/etc/openstack-catalog',
+) inherits ::apps_site::params {
+
+  class { '::apache':
+    mpm_module    => false,
+    default_vhost => false,
+    purge_configs => false,
+  }
+
+  if $use_ssl {
+    include ::apache::mod::ssl
+
+    if $ssl_cert_file_content != undef {
+      file { $ssl_cert_file_location:
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => $ssl_cert_file_content,
+        before  => Apache::Vhost['openstack-app-catalog-ssl'],
+      }
+    }
+
+    if $ssl_key_file_content != undef {
+      file { $ssl_key_file_location:
+        owner   => 'root',
+        group   => 'ssl-cert',
+        mode    => '0640',
+        content => $ssl_key_file_content,
+        before  => Apache::Vhost['openstack-app-catalog-ssl'],
+      }
+    }
+
+    if $ssl_ca_file_content != undef {
+      file { $ssl_ca_file_location:
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0640',
+        content => $ssl_ca_file_content,
+        before  => Apache::Vhost['openstack-app-catalog-ssl'],
+      }
+    }
+
+    ::apache::vhost { 'openstack-app-catalog-ssl':
+      ensure                      => 'present',
+      ssl                         => true,
+      servername                  => $servername,
+      port                        => $https_port,
+      ip                          => $bind_ip,
+      docroot                     => '/var/www',
+      priority                    => '50',
+      access_log_file             => 'app_catalog_ssl_access.log',
+      error_log_file              => 'app_catalog_ssl_error.log',
+      ssl_cert                    => $ssl_cert_file_location,
+      ssl_key                     => $ssl_key_file_location,
+      ssl_ca                      => $ssl_ca_file_location,
+      setenv                      => ["LOCAL_SETTINGS_PATH ${settings_dir}"],
+      aliases                     => [{
+        alias => '/static',
+        path  => "${apps_site::params::app_catalog_dir}/web/static",
+      }],
+      wsgi_script_aliases         => hash(['/', "${apps_site::params::app_catalog_dir}/wsgi.py"]),
+      wsgi_daemon_process         => "${apps_site::params::wsgi_group}-ssl",
+      wsgi_process_group          => "${apps_site::params::wsgi_group}-ssl",
+      wsgi_daemon_process_options => {
+        processes => $apps_site::params::wsgi_processes,
+        threads   => $apps_site::params::wsgi_threads,
+        user      => $apps_site::params::wsgi_user,
+        group     => $apps_site::params::wsgi_group,
+      },
+    }
+
+    ::apache::vhost { 'openstack-app-catalog':
+      servername                  => $servername,
+      port                        => $http_port,
+      ip                          => $bind_ip,
+      redirect_status             => 'permanent',
+      redirect_dest               => "https://${servername}/",
+      docroot                     => '/var/www',
+      priority                    => '50',
+      access_log_file             => 'app_catalog_access.log',
+      error_log_file              => 'app_catalog_error.log',
+      setenv                      => ["LOCAL_SETTINGS_PATH ${settings_dir}"],
+      aliases                     => [{
+        alias => '/static',
+        path  => "${apps_site::params::app_catalog_dir}/web/static",
+      }],
+      wsgi_script_aliases         => hash(['/', "${apps_site::params::app_catalog_dir}/wsgi.py"]),
+      wsgi_daemon_process         => $apps_site::params::wsgi_group,
+      wsgi_process_group          => $apps_site::params::wsgi_group,
+      wsgi_daemon_process_options => {
+        processes => $apps_site::params::wsgi_processes,
+        threads   => $apps_site::params::wsgi_threads,
+        user      => $apps_site::params::wsgi_user,
+        group     => $apps_site::params::wsgi_group,
+      },
+    }
+  }else{
+
+    ::apache::vhost { 'openstack-app-catalog':
+      servername                  => $servername,
+      port                        => $http_port,
+      ip                          => $bind_ip,
+      docroot                     => '/var/www',
+      priority                    => '50',
+      access_log_file             => 'app_catalog_access.log',
+      error_log_file              => 'app_catalog_error.log',
+      setenv                      => ["LOCAL_SETTINGS_PATH ${settings_dir}"],
+      aliases                     => [{
+        alias => '/static',
+        path  => "${apps_site::params::app_catalog_dir}/web/static",
+      }],
+      wsgi_script_aliases         => hash(['/', "${apps_site::params::app_catalog_dir}/wsgi.py"]),
+      wsgi_daemon_process         => $apps_site::params::wsgi_group,
+      wsgi_process_group          => $apps_site::params::wsgi_group,
+      wsgi_daemon_process_options => {
+        processes => $apps_site::params::wsgi_processes,
+        threads   => $apps_site::params::wsgi_threads,
+        user      => $apps_site::params::wsgi_user,
+        group     => $apps_site::params::wsgi_group,
+      },
+    }
+  }
+}
