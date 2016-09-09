a575dc607477832562086565aa7309448d03d838
Bartosz Kupidura <bkupidura@mirantis.com>
2016-09-09 14:47:43 +0000

    [wip] Glare support for app-catalog
    This commit should allow to deploy app-catalog with glare support
    introduced by Ib5c6920c90f566d73a5f9e8ed545f0e082a0b20e
    
    Change-Id: I9fbe756cfcc2456587c0395e88f14681a1e43ee4

---
+++ examples/install.pp
@@ -0,0 +1,22 @@
+$domain_name = '172.16.43.253'
+$port = 8000
+$glare_url = 'http://127.0.0.1:9494'
+$memcache_server = '127.0.0.1:11211'
+$import_assets = false
+
+class { '::apps_site::catalog':
+  domain          => $domain_name,
+  port            => $port,
+  glare_url       => $glare_url,
+  memcache_server => $memcache_server,
+}
+
+class { '::apps_site::wsgi::apache':
+  servername => $domain_name,
+  http_port  => $port,
+}
+
+class { '::apps_site::plugins::glare':
+  glare_url     => $glare_url,
+  import_assets => $import_assets,
+}
